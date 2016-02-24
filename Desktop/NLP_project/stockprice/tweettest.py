158cf36c433e0a218879de9871c744c7e4cc319e
sooalim77 <sooalim77@users.noreply.github.com>
2016-02-23 20:33:56 -0500

    Update tweettest.py

--- Desktop/NLP_project/stockprice/tweettest.py
+++ Desktop/NLP_project/stockprice/tweettest.py
@@ -2,10 +2,11 @@ from twython import Twython
 
 # using Twython for querying twitter via Search API 
 
-TWITTER_APP_KEY = 'GH09zTCmgzTY5EB4U4wHP6jdG'
+TWITTER_APP_KEY = 'GH09zTCmgzTY5EB4U4wHP6jdG' 
 TWITTER_APP_KEY_SECRET = 'JdMUeCxsQdwOaQPdiieLWEddVybGlHMLZ5Sk04AAPdcI2ZmXyl'
 TWITTER_ACCESS_TOKEN = '3040614638-b5YAY9dXnHUnOC6zcwqh5AypggTrzj6vJMpiau7'
 TWITTER_ACCESS_TOKEN_SECRET = 'KDlimmrvr6tWWk5uyF0X1aIijuLnMY16HCpHIE3I6y3Gb'
+
 
 # Obtain an OAuth2 Access Token
 twitter = Twython(TWITTER_APP_KEY, TWITTER_APP_KEY_SECRET, oauth_version=2)
